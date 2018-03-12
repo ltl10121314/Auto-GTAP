f23deb57570d056958dfafcafc6f0579455287fd
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-03-12 15:23:13 -0400

    Added yaml for defining control variables for the program

---
+++ Main.py
@@ -0,0 +1,17 @@
+__author__ = "Andre Barbe"
+__project__ = "GTAP-E-Validation"
+__created__ = "2018-3-9"
+__altered__ = "2018-3-12"
+
+# Import methods
+# Import External Methods
+import yaml
+
+# Import My Methods
+
+# Import control variables from yaml
+with open('config.yaml', 'r') as f:
+    config = yaml.load(f)
+# Define control variables from yaml
+solution_method = config["solution_method"]
+gtap_file_name = config["gtap_file_name"]
