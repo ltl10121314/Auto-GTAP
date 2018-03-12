2ab1772ce54724559f9767de670a8c2f003adf3e
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-03-12 18:00:08 -0400

    Added CopyInputFiles class

---
+++ CopyInputFiles.py
@@ -0,0 +1,33 @@
+__author__ = "Andre Barbe"
+__project__ = "GTAP-E-Validation"
+__created__ = "2018-3-12"
+__altered__ = "2018-3-12"
+
+import shutil
+
+
+class CopyInputFiles(object):
+    """Copies files from the input files directory to the work files directory"""
+
+    # __slots__ = []
+
+    def create(self) -> None:
+        # Define lists of files to copy.
+        files_gtap = [
+            "gtap3.tab",
+            "basedata.har",
+            "baserate.har",
+            "baseview.har",
+            "sets.har",
+            "co2.har",
+            "default.prm"
+        ]
+
+        files_other = [
+        ]
+
+        list_of_files_to_copy = files_gtap + files_other
+
+        for file_name in list_of_files_to_copy:
+            # copy files with file_name from Input_Files to Work_Files
+            shutil.copy('Input_Files\\{0}'.format(file_name), 'Work_Files\\{0}'.format(file_name))
