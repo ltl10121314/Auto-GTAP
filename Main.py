d926695d867b366438ff921e42d156242a1d290f
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-03-13 12:42:08 -0400

    Added Simulation CMF. GEMSIM now runs to completion

--- Main.py
+++ Main.py
@@ -11,6 +11,7 @@ import yaml, os, subprocess
 from CleanWorkFiles import CleanWorkFiles
 from CopyInputFiles import CopyInputFiles
 from CreateSTI import CreateSTI
+from SimulationCMF import SimulationCMF
 
 # Import control variables from yaml
 with open('config.yaml', 'r') as f:
@@ -18,11 +19,13 @@ with open('config.yaml', 'r') as f:
 # Define control variables from yaml
 solution_method = config["solution_method"]
 gtap_file_name = config["gtap_file_name"]
+simulation_name = "hello"
 
 # Call Methods
 # Setup files for running GEMSIM
 CleanWorkFiles().create()
 CopyInputFiles().create()
+SimulationCMF("sim", simulation_name, "default_{0}".format(solution_method)).create(simulation_name)
 
 # Run Simulation
 # Change working directory to Work_Files so all output (and logs) will go there when gemsim or sltoht is called
@@ -30,3 +33,4 @@ os.chdir("Work_Files")
 # Create GSS and GST files for shocks and model gemsim
 CreateSTI(gtap_file_name, "NA", "gtap").create()
 subprocess.call("tablo -sti {0}.sti".format(gtap_file_name))
+subprocess.call("gemsim -cmf sim_{0}.cmf".format(simulation_name))
