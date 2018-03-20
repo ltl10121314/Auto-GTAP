ba557562b7111a8b5de1d74bdc7bf490f0930feb
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-03-20 17:08:25 -0400

    Made changes to yaml config but not much progres in getting it to work

--- CreateConfig.py
+++ CreateConfig.py
@@ -18,9 +18,11 @@ class CreateConfig(object):
         # Import control variables from yaml
         with open(self.yaml_file_location, 'r') as f:
             yaml_file = yaml.load(f)
+
         # Define control variables from yaml
-        self.solution_method = yaml_file["solution_method"]
-        self.gtap_file_name = yaml_file["gtap_file_name"]
-        self.input_directory_list = yaml_file["input_directory_list"]
+
+        self.solution_method = yaml_file["Validate GTAP-E"]["solution_method"]
+        self.gtap_file_name = yaml_file["Validate GTAP-E"]["gtap_file_name"]
+        self.input_directory_list = yaml_file["Validate GTAP-E"]["input_directory_list"]
         self.simulation_name = "hello"
         self.simulation_list = [self.simulation_name]
