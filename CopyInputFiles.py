66e9857c5696b1efa713dcce72d4d0043bb5e325
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-03-23 15:27:20 -0400

    Changed CopyInputFiles to run only on a single simulation instead of whole list and also to allow the input and work directories to have different names

--- CopyInputFiles.py
+++ CopyInputFiles.py
@@ -1,7 +1,7 @@
 __author__ = "Andre Barbe"
 __project__ = "GTAP-E Validation"
 __created__ = "2018-3-12"
-__altered__ = "2018-3-13"
+__altered__ = "2018-3-23"
 
 import shutil
 
@@ -9,14 +9,12 @@ import shutil
 class CopyInputFiles(object):
     """Copies files from the input files directory to the work files directory"""
 
-    __slots__ = ["input_directory_list"]
+    __slots__ = ["input_folder", "simulation_name"]
 
-    def __init__(self, input_directory_list: list) -> None:
-        self.input_directory_list = input_directory_list
+    def __init__(self, input_folder: str, simulation_name: str) -> None:
+        self.input_folder = input_folder
+        self.simulation_name = simulation_name
 
     def create(self) -> None:
-        # Define lists of files to copy.
-
-        for folder in self.input_directory_list:
-            # copy files with file_name from Input_Files to Work_Files
-            shutil.copytree('Input_Files\\{0}'.format(folder), 'Work_Files\\{0}'.format(folder))
+        # copy files with file_name from Input_Files to Work_Files in folder simulation_name
+        shutil.copytree('Input_Files\\{0}'.format(self.input_folder), 'Work_Files\\{0}'.format(self.simulation_name))
