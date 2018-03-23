d58a8c05b4db60380a9ce8328fbcd5253953ebbb
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-03-23 15:54:50 -0400

    Changed methods to objects for CreateConfig

--- CreateConfig.py
+++ CreateConfig.py
@@ -9,20 +9,18 @@ import yaml
 class CreateConfig(object):
     """Creates config information from yaml file"""
 
-    __slots__ = ["yaml_file_location", "yaml_file"]
+    __slots__ = ["yaml_file_location", "yaml_file", "simulation_list", "input_directory_list"]
 
     def __init__(self, yaml_file_location: str) -> None:
         with open(yaml_file_location, 'r') as f:
             self.yaml_file = yaml.load(f)
 
-    def simulation_list(self):
-        return self.yaml_file["simulations_to_run"]
+        self.simulation_list = self.yaml_file["simulations_to_run"]
 
-    def input_directory_list(self):
-        list = []
-        for simulation in self.simulation_list():
-            list.append(self.sim_property(simulation, "input_directory"))
-        return list
+        self.input_directory_list = []
+        for simulation in self.simulation_list:
+            self.input_directory_list.append(self.sim_property(simulation, "input_directory"))
+
 
     def sim_property(self, simulation_name: str, property_name: str):
         return self.yaml_file["simulations"][simulation_name][property_name]
