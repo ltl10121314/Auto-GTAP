446b65ae865695bb5e03d68ccfbc3970a667e4f3
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-04-24 10:38:42 -0400

    CleanWorkFiles was leaving old Work_Files subfolders if they were not part of the current simulation. Now it completely deletes the contents of Work_Files

--- CleanWorkFiles.py
+++ CleanWorkFiles.py
@@ -1,36 +1,24 @@
 __author__ = "Andre Barbe"
 __project__ = "Auto-GTAP"
 __created__ = "2018-3-13"
-__altered__ = "2018-3-23"
+__altered__ = "2018-4-24"
 
 import os, shutil
 
 
 class CleanWorkFiles(object):
-    """Delete files from work directory and subfolder in it"""
+    """Deletes contents of Work_Files"""
 
-    __slots__ = ["input_directory_list"]
-
-    def __init__(self, input_directory_list: list) -> None:
-        self.input_directory_list = input_directory_list
-
-        # Delete folders in work_files
-        folders_to_delete = ['Work_Files//{0}'.format(folder) for folder in self.input_directory_list]
-        for folder in folders_to_delete:
-            # the try/except structure prevents an error if the folder does not exist
+    def __init__(self) -> None:
+        # From https://stackoverflow.com/questions/185936/how-to-delete-the-contents-of-a-folder-in-python
+        for the_file in os.listdir('Work_Files'):  # for all entries in Work_Files
+            file_path = os.path.join('Work_Files', the_file)
             try:
-                shutil.rmtree(folder)
-            except OSError:
-                pass
-
-        #Delete files in work files
-        folder = 'Work_Files'
-        for the_file in os.listdir(folder):
-            file_path = os.path.join(folder, the_file)
-            # the try/except structure prevents an error if the folder does not exist
-            try:
+                # deletes entry if it is a file
                 if os.path.isfile(file_path):
                     os.unlink(file_path)
-                    # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+                # deletes entry if it is a folder
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
             except Exception as e:
                 print(e)
