493bb524234e11dcecd3adb655d42874c589374d
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-04-30 13:59:24 -0400

    Updated CreateSTI to run with GTAP v7

--- Main.py
+++ Main.py
@@ -31,9 +31,10 @@ for simulation_name in config.simulation_list:
     CopyInputFiles(simulation_name,config.subfolders_to_copy(simulation_name))
     SimulationCMF("sim", simulation_name, "default_{0}".format(config.sim_property(simulation_name, "solution_method")),
                   config.sim_property(simulation_name, "input_directory"), config.sim_property(simulation_name, "shock")).create("Gas")
-    AggregateModelData(simulation_name)
-    MoveDatabaseFiles(simulation_name, "GTPAg2", "MSplitCom-Exe")
-    SplitCommodities(simulation_name)
+    # AggregateModelData(simulation_name)
+    # MoveDatabaseFiles(simulation_name, "GTPAg2", "MSplitCom-Exe")
+    # SplitCommodities(simulation_name)
+    # MoveDatabaseFiles(simulation_name, "MSplitCom-Exe", "GTAPV7")
     if config.sim_property(simulation_name, "modify_har"):
         ModifyHAR("Work_Files\\" + simulation_name, "olddefault", "default",
                   config.yaml_file["parameter_modifications"][
@@ -44,7 +45,8 @@ for simulation_name in config.simulation_list:
 for simulation_name in config.simulation_list:
     os.chdir("Work_Files\\{0}\\{1}".format(simulation_name,config.sim_property(simulation_name, "input_directory")))
     # Create GSS and GST files for shocks and model gemsim
-    CreateSTI(config.sim_property(simulation_name, "model_file_name"), "NA", "gtap")
+    CreateSTI(config.sim_property(simulation_name, "model_file_name"), "NA",
+              config.sim_property(simulation_name, "model_type"))
     subprocess.call("tablo -sti {0}.sti".format(config.sim_property(simulation_name, "model_file_name")))
     subprocess.call("gemsim -cmf sim_{0}.cmf".format(simulation_name))
 
