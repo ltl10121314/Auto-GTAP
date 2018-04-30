cfc29f4a3ec38a4690b008796d40f5298ff35745
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-04-30 15:24:48 -0400

    Fixed dates in file headers

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 __author__ = "Andre Barbe"
 __project__ = "Auto-GTAP"
 __created__ = "2018-3-9"
-__altered__ = "2018-4-27"
+__altered__ = "2018-4-30"
 
 # Import methods
 # Import External Methods
--- SimulationCMF.py
+++ SimulationCMF.py
@@ -1,7 +1,7 @@
 __author__ = "Andre Barbe"
 __project__ = "Auto-GTAP"
 __created__ = "2018-3-13"
-__altered__ = "2018-4-9"
+__altered__ = "2018-4-30"
 
 from SimulationShocks import SimulationShocks
 
