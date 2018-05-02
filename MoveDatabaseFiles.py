d91d4c5d76972b84e109b6969e2dd0da06a2a5cf
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-05-02 17:19:46 -0400

    Added GTAP V6 and V7 as source types

--- MoveDatabaseFiles.py
+++ MoveDatabaseFiles.py
@@ -24,36 +24,44 @@ class MoveDatabaseFiles(object):
         destination_part_folder = "Work_Files\\" + self.simulation_name + "\\" + self.destination_part_folder + "\\"
 
         if self.source_type == "GTPAg2":
-            flexagg_output_folder = source_part_folder + "GTAP10p2\\GTAP\\output"
-            source_flows_file = "{0}\\basedata.har".format(flexagg_output_folder)
-            source_parameters_file = "{0}\\default.prm".format(flexagg_output_folder)
-            source_sets_file = "{0}\\sets.har".format(flexagg_output_folder)
-            source_tax_rates_file = "{0}\\baserate.har".format(flexagg_output_folder)
-            source_view_file = "{0}\\baseview.har".format(flexagg_output_folder)
+            flexagg_output_folder = source_part_folder + "GTAP10p2\\GTAP\\output\\"
+            source_flows_file = "{0}basedata.har".format(flexagg_output_folder)
+            source_parameters_file = "{0}default.prm".format(flexagg_output_folder)
+            source_sets_file = "{0}sets.har".format(flexagg_output_folder)
+            source_tax_rates_file = "{0}baserate.har".format(flexagg_output_folder)
+            source_view_file = "{0}baseview.har".format(flexagg_output_folder)
 
         if self.source_type == "MSplitCom-Exe":
-            source_folder = source_part_folder + "\\output"
-            source_flows_file = "{0}\\basedata.har".format(source_folder)
-            source_parameters_file = "{0}\\default.prm".format(source_folder)
-            source_sets_file = "{0}\\sets.har".format(source_folder)
-            source_tax_rates_file = "{0}\\taxrates.har".format(source_folder)
-            source_view_file = "{0}\\gtapview.har".format(source_folder)
+            source_folder = source_part_folder + "output\\"
+            source_flows_file = "{0}basedata.har".format(source_folder)
+            source_parameters_file = "{0}default.prm".format(source_folder)
+            source_sets_file = "{0}sets.har".format(source_folder)
+            source_tax_rates_file = "{0}taxrates.har".format(source_folder)
+            source_view_file = "{0}gtapview.har".format(source_folder)
 
         if self.destination_type == "MSplitCom-Exe":
-            destination_folder = destination_part_folder + "\\input"
-            destination_flows_file = "{0}\\basedata.har".format(destination_folder)
-            destination_parameters_file = "{0}\\default.prm".format(destination_folder)
-            destination_sets_file = "{0}\\sets.har".format(destination_folder)
-            destination_tax_rates_file = "{0}\\baserate.har".format(destination_folder)
-            destination_view_file = "{0}\\baseview.har".format(destination_folder)
+            destination_folder = destination_part_folder + "input\\"
+            destination_flows_file = "{0}basedata.har".format(destination_folder)
+            destination_parameters_file = "{0}default.prm".format(destination_folder)
+            destination_sets_file = "{0}sets.har".format(destination_folder)
+            destination_tax_rates_file = "{0}baserate.har".format(destination_folder)
+            destination_view_file = "{0}baseview.har".format(destination_folder)
 
         if self.destination_type == "GTAP-V7" or self.destination_type == "GTAP-V6":
             destination_folder = destination_part_folder
-            destination_flows_file = "{0}\\basedata.har".format(destination_folder)
-            destination_parameters_file = "{0}\\default.prm".format(destination_folder)
-            destination_sets_file = "{0}\\sets.har".format(destination_folder)
-            destination_tax_rates_file = "{0}\\baserate.har".format(destination_folder)
-            destination_view_file = "{0}\\baseview.har".format(destination_folder)
+            destination_flows_file = "{0}basedata.har".format(destination_folder)
+            destination_parameters_file = "{0}default.prm".format(destination_folder)
+            destination_sets_file = "{0}sets.har".format(destination_folder)
+            destination_tax_rates_file = "{0}baserate.har".format(destination_folder)
+            destination_view_file = "{0}baseview.har".format(destination_folder)
+    
+        if self.source_type == "GTAP-V7" or self.source_type == "GTAP-V6":
+            source_folder = source_part_folder
+            source_flows_file = "{0}sim_GTAP-V6.har".format(source_folder)
+            source_parameters_file = "{0}default.prm".format(source_folder)
+            source_sets_file = "{0}sets.har".format(source_folder)
+            source_tax_rates_file = "{0}baserate.har".format(source_folder) #needs to be updated
+            source_view_file = "{0}baseview.har".format(source_folder) #needs to be updated
 
         self.files = {
             "flows": [source_flows_file, destination_flows_file],
