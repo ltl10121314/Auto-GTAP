bc315bf9bd037174ab528527664f8a14e6021143
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-05-02 15:44:11 -0400

    For MoveDatabaseFiles, still working on using custom working_folder names

--- MoveDatabaseFiles.py
+++ MoveDatabaseFiles.py
@@ -9,18 +9,22 @@ import shutil
 class MoveDatabaseFiles(object):
     """Moves files. Typically moves the output of one module to the input of another module"""
 
-    __slots__ = ["source_type", "destination_type", "files", "simulation_name", "work_directory"]
+    __slots__ = ["source_type", "destination_type", "files", "simulation_name", "source_part_folder",
+                 "destination_part_folder"]
 
-    def __init__(self, simulation_name: str, source_type: str, destination_type: str, work_directory: str) -> None:
+    def __init__(self, simulation_name: str, source_type: str, destination_type: str, source_part_folder: str,
+                 destination_part_folder: str) -> None:
         self.simulation_name = simulation_name
         self.source_type = source_type
         self.destination_type = destination_type
+        self.source_part_folder = source_part_folder
+        self.destination_part_folder = destination_part_folder
 
-        source_directory = "Work_Files\\" + self.simulation_name + "\\" + self.work_directory + "\\"
-        destination_directory = "Work_Files\\" + self.simulation_name + "\\" + self.work_directory + "\\"
+        source_part_folder = "Work_Files\\" + self.simulation_name + "\\" + self.source_part_folder + "\\"
+        destination_part_folder = "Work_Files\\" + self.simulation_name + "\\" + self.destination_part_folder + "\\"
 
         if self.source_type == "GTPAg2":
-            flexagg_output_folder = work_directory + "GTAP10p2\\GTAP\\output"
+            flexagg_output_folder = source_part_folder + "GTAP10p2\\GTAP\\output"
             source_flows_file = "{0}\\basedata.har".format(flexagg_output_folder)
             source_parameters_file = "{0}\\default.prm".format(flexagg_output_folder)
             source_sets_file = "{0}\\sets.har".format(flexagg_output_folder)
@@ -28,7 +32,7 @@ class MoveDatabaseFiles(object):
             source_view_file = "{0}\\baseview.har".format(flexagg_output_folder)
 
         if self.source_type == "MSplitCom-Exe":
-            source_folder = work_directory + "\\output"
+            source_folder = source_part_folder + "\\output"
             source_flows_file = "{0}\\basedata.har".format(source_folder)
             source_parameters_file = "{0}\\default.prm".format(source_folder)
             source_sets_file = "{0}\\sets.har".format(source_folder)
@@ -36,7 +40,7 @@ class MoveDatabaseFiles(object):
             source_view_file = "{0}\\gtapview.har".format(source_folder)
 
         if self.destination_type == "MSplitCom-Exe":
-            destination_folder = work_directory + "\\" + self.destination_folder + "\\input"
+            destination_folder = destination_part_folder + "\\input"
             destination_flows_file = "{0}\\basedata.har".format(destination_folder)
             destination_parameters_file = "{0}\\default.prm".format(destination_folder)
             destination_sets_file = "{0}\\sets.har".format(destination_folder)
@@ -44,7 +48,7 @@ class MoveDatabaseFiles(object):
             destination_view_file = "{0}\\baseview.har".format(destination_folder)
 
         if self.destination_type == "GTAP-V7" or self.destination_type == "GTAP-V6":
-            destination_folder = work_directory + self.destination_folder
+            destination_folder = destination_part_folder
             destination_flows_file = "{0}\\basedata.har".format(destination_folder)
             destination_parameters_file = "{0}\\default.prm".format(destination_folder)
             destination_sets_file = "{0}\\sets.har".format(destination_folder)
