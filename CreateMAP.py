7c0621c1a4c9726c97a49d60314a43600b7e5ff9
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-05-03 11:43:02 -0400

    Moved remaining map information from CreateMAP to config.yaml

--- CreateMAP.py
+++ CreateMAP.py
@@ -1,7 +1,7 @@
 __author__ = "Andre Barbe"
 __project__ = "Auto-GTAP"
 __created__ = "2018-3-13"
-__altered__ = "2018-4-30"
+__altered__ = "2018-5-3"
 
 
 class CreateMAP(object):
@@ -15,27 +15,7 @@ class CreateMAP(object):
         self.simulation_name = simulation_name
         self.map_lines = map_lines
 
-        line_list_gas = [
-            'pm("Gas","USA")\n',
-            'aoall("Gas","USA")\n',
-            'qo("Gas","USA")\n',
-            'qxw("Gas","USA")\n',
-            'qiw("Gas","USA")\n'
-        ]
-
-        line_list_oil = [
-            'pm("Oil","USA")\n',
-            'aoall("Oil","USA")\n',
-            'qo("Oil","USA")\n',
-            'qxw("Oil","USA")\n',
-            'qiw("Oil","USA")\n'
-        ]
-
-        # line_list_total = ['pms("c_Crops","EU_28")\n']
-        # line_list_total = ['pxwreg("EU_28")\n']
-        line_list_total = self.map_lines
-
         # Create final file
         file_name = self.project_name + "_" + self.simulation_name
         with open("{0}.map".format(file_name), "w+") as writer:  # Create the empty file
-            writer.writelines(line_list_total)  # write the line list to the file
+            writer.writelines(self.map_lines)  # write the line list to the file
