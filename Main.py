994dcd369a0379073515b2171acda3e003539321
Andre-Barbe <31875393+andre-barbe@users.noreply.github.com>
2018-07-19 14:02:04 -0400

    Revise comments and delete unused variables

--- Main.py
+++ Main.py
@@ -1,50 +1,45 @@
-import os
-# noinspection PyPep8Naming
-import AutoGTAP as ag
+import AutoGTAP as Ag
 
 # Call Methods
 # Load config files that will control program
-config = ag.CreateConfig("config.yaml")
+config = Ag.CreateConfig("config.yaml")
 # Delete working files directory
-ag.CleanWorkFiles()
+Ag.CleanWorkFiles()
 # For each simulation, perform the different subparts (data aggregation, splitting,
 # experiment simulation, etc) that make up that simulation
 for simulation_name in config.simulation_list:
     # Add one to final range so that python will run the last part too
     for part_num in range(1, config.num_parts(simulation_name) + 1):
 
-        # Load configuration information for this particular part
         part_type = config.yaml_file["simulations"][simulation_name]["subparts"][part_num]["type"]
-        part_input_folder = config.yaml_file["simulations"][simulation_name]["subparts"][part_num]["input_folder"]
-        part_work_folder = config.yaml_file["simulations"][simulation_name]["subparts"][part_num]["work_folder"]
 
         # Copy input files for this part to the appropriate work directory
-        ag.CopyInputFiles(config, simulation_name, part_num).copy()
+        Ag.CopyInputFiles(config, simulation_name, part_num).copy()
 
         # Copy Work files from the previous part to the work directory for this part, unless this is the first part
         if part_num != 1:
-            ag.MoveFilesBetweenSteps(config, simulation_name, part_num)
+            Ag.MoveFilesBetweenSteps(config, simulation_name, part_num)
 
         # Run the actual work for this part, depending on which type of part it is
         if part_type == "GTPAg2":
-            ag.AggregateModelData(config, simulation_name, part_num)
+            Ag.AggregateModelData(config, simulation_name, part_num)
 
         elif part_type == "MSplitCom-Exe":
-            ag.SplitCommodities(simulation_name)
+            Ag.SplitCommodities(simulation_name)
 
         elif part_type == "modify_har":
-            ag.ModifyHAR(config, simulation_name, part_num)
+            Ag.ModifyHAR(config, simulation_name, part_num)
 
         elif part_type == "GTPVEW-V6" or part_type == "Shocks-V6":
-            ag.Gtpvew(config, simulation_name, part_num)
+            Ag.Gtpvew(config, simulation_name, part_num)
 
         elif part_type == "GTAP-Adjust":
-            ag.GTAPAdjustCMF(config, simulation_name, part_num)
+            Ag.GTAPAdjustCMF(config, simulation_name, part_num)
 
         elif part_type == "GTAP-V6" or part_type == "GTAP-E":
-            ag.GtapModel(config, simulation_name, part_num)
+            Ag.GtapModel(config, simulation_name, part_num)
 
         else:
             raise ValueError('Unexpected part type: %s' % part_type)
 
-ag.ExportResults(config)
+Ag.ExportResults(config)
--- src/AutoGTAP/ExportResults/ExportResults.py
+++ src/AutoGTAP/ExportResults/ExportResults.py
@@ -10,7 +10,7 @@ from AutoGTAP.CreateOutput.CreateOutput import CreateOutput
 
 
 class ExportResults(object):
-    """Runs the GTAP model itself"""
+    """Exports the results of all the simulations into a single CSV file"""
 
     __slots__ = ["config"]
 
@@ -19,11 +19,11 @@ class ExportResults(object):
         old_work_directory = os.getcwd()
         os.chdir("WorkFiles")
         # load the various CSV files created by the experiment simulation into a database
-        databaseSL4 = ImportCsvSl4(config.csv_paths()).create()
+        database_sl4 = ImportCsvSl4(config.csv_paths()).create()
         # Modify the database to make it more readable
-        databaseMod = ModifyDatabase(databaseSL4).create()
+        database_mod = ModifyDatabase(database_sl4).create()
         # Export the database to a results csv file
-        ExportDictionary("Results.csv", databaseMod)
+        ExportDictionary("Results.csv", database_mod)
         # Copy results.csv to the OutputFiles directory, and rename it with a timestamp
         os.chdir(old_work_directory)
         CreateOutput()
